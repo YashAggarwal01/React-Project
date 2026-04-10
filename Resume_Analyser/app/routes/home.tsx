30f0347a024286975a2a3c0d478415368b165972
YashAggarwal01 <mr.yashgamerzyt@gmail.com>
2026-04-10 23:04:56 +0530

    card alignment fix

--- Resume_Analyser/app/routes/home.tsx
+++ Resume_Analyser/app/routes/home.tsx
@@ -14,17 +14,19 @@ export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navvbar/>
     <section className="main-section">
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Track Your Application & Resume Ratings</h1>
         <h2>Review Your submission and check AI-Powered feedback</h2>
       </div>
+    
+
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume}/>
+          ))}
+        </div>
+      )}
     </section>
-
-
-    {resumes.map((resume) => (
-      <ResumeCard key={resume.id} resume={resume}/>
-    ))}
-
-
   </main>;
 }
